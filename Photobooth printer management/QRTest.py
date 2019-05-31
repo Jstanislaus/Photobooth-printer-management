befe14acac73538ca505c4aa6c34d8229613c510
Jacob <jacob@stanislaus.co.uk>
2019-05-31 19:40:09 +0100

    .

--- Photobooth printer management/QRTest.py
+++ Photobooth printer management/QRTest.py
@@ -1,15 +1,18 @@
 import qrcode
 import datetime
+import os
+
+
 
 starttime = (datetime.datetime.now())
 
 QRDdata = "Here is some QRCodeTada!"
-QRFilename = "QRCode.jpg"
+QRFilename =  os.path.join('Temp', "QRCode.jpg") #Path of template image
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
+    box_size=3,
+    border=1,
 )
 qr.add_data(QRDdata)
 qr.make(fit=True)
