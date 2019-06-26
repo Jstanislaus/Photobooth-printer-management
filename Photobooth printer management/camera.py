df4265bb46f2c553b0964c9bc438ae181f473f79
adria <adria@HP-OFFICE>
2019-06-26 08:11:18 +0100

    .

--- Photobooth printer management/camera.py
+++ Photobooth printer management/camera.py
@@ -509,7 +509,7 @@ def TakePictures():
     Final_Image_Name = os.path.join(os.path.realpath(imagefolder), "Final_" + str(TotalImageCount)+"_"+str(ts) + ".jpg")
     print(Final_Image_Name)
 
-    QRData = "Final_" + str(TotalImageCount)+"_"+str(ts)
+    QRDdata = "Final_" + str(TotalImageCount)+"_"+str(ts)
     starttime = (datetime.datetime.now())
 
     #QRDdata = "Here is some QRCodeTada!"
