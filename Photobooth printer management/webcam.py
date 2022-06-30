a88fd91eb045993f856e50f9477db9270bcbe570
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2022-06-30 19:40:01 +0100

    Update webcam.py

--- Photobooth printer management/webcam.py
+++ Photobooth printer management/webcam.py
@@ -574,7 +574,14 @@ def TakePictures():
     #image2 = PIL.Image.open(filename2)
     image2 = Image.open(filename2)
     height,width = image2.size
-    image2 = image2.crop(((int(width)/2)-300,int((height)/2)-200,600,400)),PIL.Image.ANTIALIAS
+    print(type(height))
+    print(type(width))
+    print(type(image2))
+    left = (width/2)-300
+    top = (height/2)-200
+    right = 600
+    bottom = 400
+    image2 = image2.crop((left,top,right,bottom))
     print("Type for image 2 is ")
     print(type(image2))
     #wpercent = (basewidth / float(image2.size[0]))
