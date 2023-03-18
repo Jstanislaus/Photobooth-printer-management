677807fb7283820145893465a72043daeb14346d
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-18 12:00:37 +0000

    Update webcam.py

--- Photobooth printer management/webcam.py
+++ Photobooth printer management/webcam.py
@@ -489,9 +489,9 @@ def CapturePicture():
         width = int(img.get_width())
         height = int(img.get_height())
         if int(width/6)*2>(height/2):
-	    step = int(height/4)
+            step = int(height/4)
         else:
-	    step = int(width/6)
+            step = int(width/6)
         left = (width/2)-(3*step)
         top = (height/2)-(2*step)
         right = 6*step
@@ -500,9 +500,9 @@ def CapturePicture():
         # Make the image full screen
 	#screenPicture.get_size()
         if int(x/6)*2>(y/2):
-                step = int(y/4)
+            step = int(y/4)
         else:
-	    step = int(x/6)
+            step = int(x/6)
         right = 6*step
         bottom = 4*step
         cropimg = pygame.transform.scale(cropimg, ((left+right),(top+bottom)))
