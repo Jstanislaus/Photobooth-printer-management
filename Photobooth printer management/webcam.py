cb44c095a650ea1f6f0705e2f4655ad43f989758
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-18 12:42:45 +0000

    Update webcam.py

--- Photobooth printer management/webcam.py
+++ Photobooth printer management/webcam.py
@@ -744,8 +744,7 @@ def TakePictures():
     bgimage.paste(QRCode, (480,280)) 
 
     # Save it to the SMB Share directory
-    bgimage = bgimage.convert("RGB")
-    bgimage.save(Final_Image_Name)
+    bgimage.save(Final_Image_Name.convert("RGB"))
     ShowPicture(Final_Image_Name,3)
 
     ImageShowed = False
