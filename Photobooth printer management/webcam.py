73a311653a51959c8da1a62f700aa5d1eaa466f6
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-22 18:39:23 +0000

    Update webcam.py

--- Photobooth printer management/webcam.py
+++ Photobooth printer management/webcam.py
@@ -190,11 +190,11 @@ def InitCamera(i):
 
 
 
-        import shlex, subprocess
-        gphoto2CmdLine = "gphoto2 --auto-detect"
-        args = shlex.split(gphoto2CmdLine)
-#        print(args)
-        gpout = subprocess.Popen(args,stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        #import shlex, subprocess
+        #gphoto2CmdLine = "gphoto2 --auto-detect"
+        #args = shlex.split(gphoto2CmdLine)
+#       # print(args)
+       # gpout = subprocess.Popen(args,stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         if i == 0:
             Message = "Waiting for camera response "
             print(Message)
@@ -202,14 +202,14 @@ def InitCamera(i):
         Message = ""
         Message2 = ""   
 
-        gpout1=gpout.wait()
+       # gpout1=gpout.wait()
 
-        CameraModel = gpout.stdout.readlines()
-        print("CAMERA MODEL1")
-        print(CameraModel)
-        del CameraModel[0:2]
-        print("CAMERA MODEL2")
-        print(CameraModel)
+       # CameraModel = gpout.stdout.readlines()
+      #  print("CAMERA MODEL1")
+      #  print(CameraModel)
+      #  del CameraModel[0:2]
+      #  print("CAMERA MODEL2")
+      #  print(CameraModel)
              
         if len(CameraModel):
             if i ==0:
