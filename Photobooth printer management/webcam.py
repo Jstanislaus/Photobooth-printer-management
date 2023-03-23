e220f34423f94d5cc867184d7c8a716d64d19b6e
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-23 18:31:58 +0000

    Update webcam.py

--- Photobooth printer management/webcam.py
+++ Photobooth printer management/webcam.py
@@ -930,7 +930,7 @@ def WaitForEvent():
         input_state = pf.input_pins[1] #False #windows10 GPIO.input(BUTTON_PIN)
 
 
-        if Not input_state: #was TRUE
+        if input_state==0: #was TRUE
             print("NoEvent is True")
             print(input_state)
             NotEvent = False
