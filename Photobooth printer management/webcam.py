2f9a315c1a8e378c3ba9317ca8ece5ff7378e0a0
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-23 19:17:23 +0000

    Update webcam.py

--- Photobooth printer management/webcam.py
+++ Photobooth printer management/webcam.py
@@ -913,8 +913,8 @@ def CheckForEvent(up,checkhori,direction):
     NotEvent = True
     #pf.read()
     #input_state = pf.read_pin(1) #False #windows10 GPIO.input(BUTTON_PIN)
-    input_stateup = pf.input_switches[0].value
-    input_statedown = pf.input_switches[1].value
+    input_stateup = pf.witches[0].value
+    input_statedown = pf.switches[1].value
     if input_stateup == 0 and (up+20)<(checkhori/2): #was TRUE
         NotEvent = False
         up += 20
