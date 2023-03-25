ca93fabf1a498306619d3817a403be4e28555bc2
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-25 17:39:08 +0000

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -5,13 +5,12 @@ import time
 import numpy as np
 
 from picamera2 import Picamera2, Preview
-print("Hi")
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration())
 picam2.start_preview(Preview.DRM)
 picam2.start()
 time.sleep(1)
-
+print("Hi")
 overlay = np.zeros((300, 400, 4), dtype=np.uint8)
 overlay[:150, 200:] = (255, 0, 0, 64)
 overlay[150:, :200] = (0, 255, 0, 64)
