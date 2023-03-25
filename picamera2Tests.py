33bfa03a24fa80458695b2c803f297b13994be18
Jacob <jacob@stanislaus.co.uk>
2023-03-25 21:11:39 +0000

    test

--- picamera2Tests.py
+++ picamera2Tests.py
@@ -1,15 +1,23 @@
-from qt_gl_preview import *
-from picamera2 import *
+#!/usr/bin/python3
+
+# Capture a JPEG while still running in the preview mode. When you
+# capture to a file, the return value is the metadata for that image.
+
 import time
 
-picam2 = Picamera2()
-preview = QtGlPreview(picam2)
+from picamera2 import Picamera2, Preview
 
-preview_config = picam2.preview_configuration()
-capture_config = picam2.still_configuration()
+picam2 = Picamera2()
+
+preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
 picam2.configure(preview_config)
+
+picam2.start_preview(Preview.QTGL)
 
 picam2.start()
 time.sleep(2)
 
-picam2.switch_mode_and_capture_file(capture_config, "capture.jpg")
+metadata = picam2.capture_file("test.jpg")
+print(metadata)
+
+picam2.close()
