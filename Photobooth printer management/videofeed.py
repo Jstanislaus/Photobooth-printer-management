16ae66dada78e67e01f3154d2db64c486508200f
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-25 17:56:35 +0000

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -1,21 +1,44 @@
+#!/usr/bin/python3
+import time
 import cv2
 import numpy as np
-import time
+from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
-from picamera2 import Picamera2
-
+#Picamera2.set_logging(Picamera2.DEBUG)
 picam2 = Picamera2()
-picam2.start(show_preview=True)
+config = picam2.create_video_configuration()
+picam2.configure(config)
+picam2.start_preview(Preview.DRM, x=0, y=0, width=1920, height=1080)
+picam2.start()
 
-for time_left in range(10, 0, -1):
-    colour = (0, 255, 0, 255)
-    origin = (0, 30)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    scale = 1
-    thickness = 2
-    overlay = np.zeros((640, 480, 4), dtype=np.uint8)
-    cv2.putText(overlay, str(time_left), origin, font, scale, colour, thickness)
-    picam2.set_overlay(overlay)
-    time.sleep(1)
+def show_texts(rec_time):
+    for time_left in range(rec_time, -1, -1):
+        colour = (255, 0, 0, 255)
+        origin = (0, 50)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        scale = 1
+        thickness = 2
+        overlay = np.zeros((640, 480, 4), dtype=np.uint8)
+        cv2.putText(overlay, str(time_left), origin, font, scale, colour, thickness)
+        picam2.set_overlay(overlay)
+        time.sleep(1)
+
+def record():
+    encoder = H264Encoder(1000000)
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    encoder.output = FfmpegOutput(timestr+".mp4")
+    picam2.start_encoder(encoder)
+    show_texts(5)
+    picam2.stop_encoder()
+
+record()
+time.sleep(1)
+record()
+time.sleep(1)
+record()
+time.sleep(1)
+record()
 
 picam2.stop()
