1f3fb414009ebc8a6b48fba949c398ee1200baa9
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 16:55:13 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -7,7 +7,8 @@ picam2 = Picamera2()
 config = picam2.create_preview_configuration({"size": (WIDTH, HEIGHT)})
 picam2.configure(config)
 
-picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)#picam2.start()
+picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
+picam2.start()
 time.sleep(10)
 
 #import cv2
