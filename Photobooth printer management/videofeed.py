7c48403161f861df8b04f4d1b858f75bd8b67224
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 16:24:42 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -9,8 +9,9 @@ HEIGHT = 1080
 
 picam2 = Picamera2()
 config = picam2.create_preview_configuration({"size": (WIDTH, HEIGHT)})
+print("test")
 picam2.configure(config)
-
+print("TEST")
 picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
 picam2.start()
 
