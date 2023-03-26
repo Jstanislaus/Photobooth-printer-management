19161f0896751a6811ec2d050973fee1511f1761
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 16:21:25 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -4,8 +4,15 @@ import time
 
 from picamera2 import Picamera2
 
+WIDTH = 1920
+HEIGHT = 1200
+
 picam2 = Picamera2()
-picam2.start(show_preview=True)
+config = picam2.create_preview_configuration({"size": (WIDTH, HEIGHT)})
+picam2.configure(config)
+
+picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
+picam2.start()
 
 for time_left in range(10, 0, -1):
     colour = (0, 255, 0, 255)
