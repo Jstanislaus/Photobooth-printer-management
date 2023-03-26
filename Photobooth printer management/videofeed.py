166fee4b8e5a3d8411e7f83c7d1d3d15a79b24d6
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 16:59:02 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -1,44 +1,36 @@
-from picamera2 import Picamera2, Preview
-import time
-WIDTH = 1920
-HEIGHT = 1080
-
-picam2 = Picamera2()
-config = picam2.create_preview_configuration({"size": (WIDTH, HEIGHT)})
-picam2.configure(config)
-
-picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
-picam2.start()
-time.sleep(10)
-
-#import cv2
-#from pyzbar.pyzbar import decode
-
-#from picamera2 import MappedArray, Picamera2, Preview
-
-#colour = (0, 255, 0)
-#font = cv2.FONT_HERSHEY_SIMPLEX
-##scale = 1
-#thickness = 2
-
-
-#def draw_barcodes(request):
-#    with MappedArray(request, "main") as m:
-#        for b in barcodes:
-#            if b.polygon:
-#                x = min([p.x for p in b.polygon])
-#                y = min([p.y for p in b.polygon]) - 30
-#                cv2.putText(m.array, b.data.decode('utf-8'), (x, y), font, scale, colour, thickness)
-
+#from picamera2 import Picamera2, Preview
+#import time
+#WIDTH = 1920
+#HEIGHT = 1080
 
 #picam2 = Picamera2()
-#picam2.start_preview(Preview.QTGL)
-#config = picam2.create_preview_configuration(main={"size": (1280, 960)})
+#config = picam2.create_preview_configuration({"size": (WIDTH, HEIGHT)})
 #picam2.configure(config)
 
-#barcodes = []
-#picam2.post_callback = draw_barcodes
+#picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
 #picam2.start()
-#while True:
- #   rgb = picam2.capture_array("main")
+#time.sleep(10)
+
+import cv2
+from pyzbar.pyzbar import decode
+
+from picamera2 import MappedArray, Picamera2, Preview
+
+colour = (0, 255, 0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+scale = 1
+thickness = 2
+
+
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+config = picam2.create_preview_configuration(main={"size": (1280, 960)})
+picam2.configure(config)
+
+barcodes = []
+#picam2.post_callback = draw_barcodes
+picam2.start()
+while True:
+    rgb = picam2.capture_array("main")
     #barcodes = decode(rgb)
