71a78ff694fb3195c5541cbf5706bd0f3cb7dc02
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 17:50:11 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -14,7 +14,7 @@ picam2.start_preview(Preview.QTGL, x=0, y=0, width=WIDTH, height=HEIGHT)
 picam2.start()
 for time_left in range(10, 0, -1):
     colour = (0, 255, 0, 255)
-    origin = (0, 0)
+    origin = (300, 200)
     font = cv2.FONT_HERSHEY_SIMPLEX
     scale = 10
     thickness = 20
