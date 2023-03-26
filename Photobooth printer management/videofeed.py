84667dcc2848cbc8222dbb9b9f06171fffad623f
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 16:26:38 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -1,7 +1,3 @@
-import cv2
-import numpy as np
-import time
-
 from picamera2 import Picamera2, Preview
 
 WIDTH = 1920
@@ -10,19 +6,6 @@ HEIGHT = 1080
 picam2 = Picamera2()
 config = picam2.create_preview_configuration({"size": (WIDTH, HEIGHT)})
 picam2.configure(config)
-print("TEST")
+
 picam2.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
 picam2.start()
-print("test")
-for time_left in range(10, 0, -1):
-    colour = (0, 255, 0, 255)
-    origin = (0, 30)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    scale = 1
-    thickness = 2
-    overlay = np.zeros((640, 480, 4), dtype=np.uint8)
-    cv2.putText(overlay, str(time_left), origin, font, scale, colour, thickness)
-    picam2.set_overlay(overlay)
-    time.sleep(1)
-
-picam2.stop()
