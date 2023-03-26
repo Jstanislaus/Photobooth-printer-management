55660081aa17427c8f5470f133fda92f3d06b985
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-26 19:06:36 +0100

    Update videofeed.py

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -21,8 +21,9 @@ for time_left in range(10, 0, -1):
     textsize = cv2.getTextSize(str(time_left), font, scale, thickness)[0]
     textX = int(616 - (textsize[0] / 2))
     textY = int(820 + (textsize[1] / 2))
-    overlay = np.zeros((1640, 1232, 4), dtype=np.uint8)
-    cv2.putText(overlay, str(time_left), (textX, textY ), font, scale, colour, thickness,lineType = cv2.LINE_AA)
+    overlay = cv2.imread("1.png", cv2.IMREAD_UNCHANGED
+    #overlay = np.zeros((1640, 1232, 4), dtype=np.uint8)
+    #cv2.putText(overlay, str(time_left), (textX, textY ), font, scale, colour, thickness,lineType = cv2.LINE_AA)
     picam2.set_overlay(overlay)
     time.sleep(1)
 
