26e40ace74722d1cc7998f09b5273c0f49a542e4
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-05-05 18:14:31 +0100

    test2

--- Photobooth_printer_management/webcam.py
+++ Photobooth_printer_management/webcam.py
@@ -611,7 +611,7 @@ def CapturePicture():
     Message2 =  ""
     Message3 =  ""
     Numeral = ""
-    filename= "/home/pi"
+    filename= "/home/pi/test.jpg"
     pygame.image.save(cropimg1,filename) 
     ShowPicture(filename, 2,Message)
     time.sleep(0.75)
