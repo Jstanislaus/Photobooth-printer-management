c1f1d31aa37a181f90294b74b616f7f4d78dbed3
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-06-27 14:22:43 +0100

    .

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -14,7 +14,7 @@ picam2.start_preview(Preview.QTGL, x=0, y=0, width=1920, height=1080)
 #picam2.create_preview_configuration()
 #picam2.start_preview(Preview.QTGL)
 picam2.start()
-
+i=0
 for time_left in range(10, 0, -1):
     colour = (0, 255, 0, 255)
     origin = (0, 30)
@@ -24,6 +24,8 @@ for time_left in range(10, 0, -1):
     overlay = np.zeros((640, 480, 4), dtype=np.uint8)
     cv2.putText(overlay, str(time_left), origin, font, scale, colour, thickness)
     picam2.set_overlay(overlay)
+    i+=1
     time.sleep(1)
 
 picam2.stop()
+print(i)
