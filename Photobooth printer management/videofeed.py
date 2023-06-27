37937b0cc3e032c584cdff516b699d11edd763cc
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-06-27 13:41:08 +0100

    .

--- Photobooth printer management/videofeed.py
+++ Photobooth printer management/videofeed.py
@@ -1,9 +1,10 @@
 from picamera2 import Picamera2, Preview
 import time
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
+config = picam2.create_preview_configuration()
+picam2.configure(config)
 picam2.start()
 time.sleep(2)
-picam2.capture_file("test.jpg")
+picam2.stop_preview()
+picam2.start_preview(True)
+time.sleep(2)
