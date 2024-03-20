70786be2657918adf180f0087f52a28aac382182
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2024-03-20 15:18:28 +0000

    .

--- webcam.py
+++ webcam.py
@@ -747,6 +747,7 @@ def TakePictures():
     # Create the final filename
     ts = time.time()
     create_folder(imagefolder+"/Final_Templated_Photos")
+    print(imagefolder+"/Final_Templated_Photos")
     Final_Image_Name = os.path.join(os.path.realpath(imagefolder),"Final_Templated_Photos","Final_" +  Venueid + str(TotalImageCount)+"_" + ".jpg")
     if VERBOSE:
         print(Final_Image_Name)
